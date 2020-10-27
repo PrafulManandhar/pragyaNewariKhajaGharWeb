5634f101cdd6515f6993c09f2e99d3faf05b5c2a
Praful Manandhar <praful.manandhar@cloudyfox.com>
2020-10-27 15:19:32 +0545

    routeing

--- src/Routes.js
+++ src/Routes.js
@@ -1,18 +1,20 @@
 import React from "react";
-import { Route, Switch, HashRouter } from "react-router-dom";
+import { Route, Switch, BrowserRouter } from "react-router-dom";
 import AboutUs from "./Pages/AboutUs";
 import Contact from "./Pages/ContactUs";
 import Main from "./Pages/Main";
 
 const Routes = () => {
   return (
-    <HashRouter>
-      <Switch>
-        <Route exact path="/" component={Main} />
-        <Route exact path="/about" component={AboutUs} />
-        <Route exact path="/contact" component={Contact} />
-      </Switch>
-    </HashRouter>
+    <>
+      <BrowserRouter>
+        <Switch>
+          <Route exact path="/" component={Main} />
+          <Route exact path="/about" component={AboutUs} />
+          <Route exact path="/contact" component={Contact} />
+        </Switch>
+      </BrowserRouter>
+    </>
   );
 };
 
